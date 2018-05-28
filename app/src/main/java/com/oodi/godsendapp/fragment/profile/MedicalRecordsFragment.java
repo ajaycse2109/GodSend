06262ce756ec43b9e1a9a5bad5c66f35e21758fe
Ajay <ajay@oodi.co.in>
2018-05-28 17:50:44 +0530

    change 1 28252018

--- app/src/main/java/com/oodi/godsendapp/fragment/profile/MedicalRecordsFragment.java
+++ app/src/main/java/com/oodi/godsendapp/fragment/profile/MedicalRecordsFragment.java
@@ -651,10 +651,10 @@ public class MedicalRecordsFragment extends RootFragment implements AdapterView.
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
+       // String item = parent.getItemAtPosition(position).toString();
 
         // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+      //  Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
 
     }
 
